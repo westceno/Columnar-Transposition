070f5afd1bf7b7282e61736a6134c028dd54a2dd
Weston Rainer <Rainerw1@mymail.nku.edu>
2019-11-17 16:19:11 -0500

    Added id's and functions for deciphers and sorting

--- FrontPage.js
+++ FrontPage.js
@@ -30,5 +30,63 @@ function CreateMatrix(keyword, message)
 
 function Sort(matrix)
 {
+	var sorted = false;
+	var lowerLimit = 0;
+	var swapLocation = 0;
+	
+	
+	while(lowerLimit <=  matrix.length)
+	{
+		var lowest = matrix[lowerLimit][0];
+
+		for(int i = lowerLimit + 1; i <= matrix.length; i++)
+		{
+			if(lowest > matrix[lowerLimit][0])
+			{
+				lowest = matrix[lowerLimit][0];
+				swapLocation = i;
+			}
+		}
+		
+		var temp = matrix[lowerLimit];
+		matrix[lowerLimit] = matrix[swapLocation];
+		matrix[swapLocation] = temp;
+		
+		lowerLimit++;
+	}
+	
+	return matrix;
+}
+
+function removeSymbols(message)
+{
+	message.replace(" ", "")
+	message.replace(",", "")
+	message.replace("'", "")
+	message.replace(".", "")
+	message.replace("-", "")
+	message.replace("/", "")
+	message.replace("\n", "")
+	message.replace("\t", "")
+	message.replace(":", "")
+	return message;
 	
 }
+
+//add function for putting it back into 5 space thing
+
+function DecipherWithKeyWord()
+{
+	var keyword = removeSymbols(document.getElementById('key').value);
+	var ct = removeSymbols(document.getElementById('ct').value);	
+	
+	var matrix = CreateMatrix(keyword, matrix);
+	
+	matrix = sort(matrix);
+	
+	//Create table
+}
+
+//Add keyword based of keyword length
+
+
